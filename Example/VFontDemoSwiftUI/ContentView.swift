0042dc3a98845654dbb23fb356ca633b8f344806
dufflink <skoryninmaksim1@gmail.com>
2022-05-11 13:49:20 +0400

    Moved SwiftUI demo target to workspace

---
+++ Example/VFontDemoSwiftUI/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  VFontDemoSwiftUI
+//
+//  Created by Maxim Skorynin on 11.05.2022.
+//
+
+import SwiftUI
+import VFont
+
+struct ContentView: View {
+    
+    @State private var widthAxisValue = 75.0
+    @State private var weightAxisValue = 100.0
+    
+    private let font = VFont(name: "Martian Mono", size: 20)!
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .font(.init(vFont: font, axes: [
+                    2003072104: CGFloat(widthAxisValue),
+                    2003265652: CGFloat(weightAxisValue)
+                ]))
+            
+            Slider(value: $widthAxisValue, in: 75...112)
+                .padding()
+            Slider(value: $weightAxisValue, in: 100...800)
+                .padding()
+        }
+        .onAppear {
+            print(font.getAxesDescription())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Example/VFontDemoSwiftUI/VFontDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VFontDemoSwiftUIApp.swift
+//  VFontDemoSwiftUI
+//
+//  Created by Maxim Skorynin on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct VFontDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
